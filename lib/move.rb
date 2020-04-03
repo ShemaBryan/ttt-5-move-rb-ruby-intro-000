29742f07263fbc0ec2072e1a6de31162af755f11
Shema Bryan <shema.bryan@jjay.cuny.edu>
2020-04-03 20:34:00 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -9,7 +9,7 @@ end
 def input_to_index(user_input)
   user_input.to_i - 1
 end
-def move(board, index, player)
+def move(board, index, player = "X")
    board[index] = player
 end
 def position_taken?(board, index)
